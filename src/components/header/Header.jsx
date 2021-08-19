1e94cc1664f4be56a3ae8a93abc8579b931029b3
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-08-19 11:09:31 -0700

    added styling and organization

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,8 @@ import styles from './Header.css';
 
 export default function Header() {
   return (
-    <div className="styles.Header">
+    <div className={styles.Header}>
       <h1>Nick Day</h1>
-      <p>This website is under construction.</p>
-      <p>Please enjoy this résumé for now.</p>
     </div>
   );
 }
