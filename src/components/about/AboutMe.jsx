a921d20c0030e3b47a80b67606df185c78609d3f
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-08-24 09:03:09 -0700

    about me page with github api

---
+++ src/components/about/AboutMe.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <>
+      <img src="" alt="" />
+      <figcaption>
+        <p>name: Nick Day</p>
+        <p>stack: </p>
+        <p>Github activity:</p>
+      </figcaption>
+    </>
+  );
+};
