d552e54fc8497535430443321a5318d3bb2ae92f
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-10-18 13:14:54 -0700

    Updated greeting

--- src/components/home/Greeting.jsx
+++ src/components/home/Greeting.jsx
@@ -5,8 +5,13 @@ function Greeting() {
   return (
     <section className={styles.greeting}>
       <h3>Hello and welcome to my Portfolio!</h3>
-      <p></p>
-      <p>Below you will find my recent projects and stuff!</p>
+      <p>
+        I am on a journey to make tools that improve quality of life for people
+        like you or me. Sometimes that means raising awareness of global warming
+        with Local Impact, helping find used furniture with Market Scrape, or
+        blowing off some steam with Tune Match.
+      </p>
+      <p>Below you will find my recent projects and more info!</p>
     </section>
   );
 }
