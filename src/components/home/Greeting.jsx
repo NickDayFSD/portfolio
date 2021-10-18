9cdf9f9ce565531426d955cf884b9a5d17ab3194
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-10-18 13:40:18 -0700

    greeting - updated text and formatting

--- src/components/home/Greeting.jsx
+++ src/components/home/Greeting.jsx
@@ -4,14 +4,22 @@ import styles from './Home.css';
 function Greeting() {
   return (
     <section className={styles.greeting}>
-      <h3>Hello and welcome to my Portfolio!</h3>
+      <h3>Hello and Welcome!</h3>
       <p>
         I am on a journey to make tools that improve quality of life for people
-        like you or me. Sometimes that means raising awareness of global warming
-        with Local Impact, helping find used furniture with Market Scrape, or
-        blowing off some steam with Tune Match.
+        like you or me.
       </p>
-      <p>Below you will find my recent projects and more info!</p>
+      <p>
+        Sometimes that means raising awareness of global warming with Local
+        Impact, helping find used furniture with Market Scrape, or blowing off
+        some steam with Tune Match.
+      </p>
+      <p>
+        My latest project is yet to be displayed here. Ravenry aims to provide
+        peace of mind for small business owners and professionals through
+        automated cyber security reporting.
+      </p>
+      <p>Follow me on LinkedIn for project updates.</p>
     </section>
   );
 }
