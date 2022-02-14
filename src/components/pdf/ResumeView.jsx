595fad8d32ca898ff452e7c72ec443e4f98493fa
NickDayFSD <Nick.Day.FSD@gmail.com>
2022-02-14 14:04:12 -0800

    dep-fix: removed react-pdf remnants react-pdf is built for webpack and is not being used currently it is causing deployment errors and has therefor been removed

--- src/components/pdf/ResumeView.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PdfViewer from './PdfViewer';
-// import resume from './NickDay.pdf';
-
-function Resume() {
-  return <PdfViewer pdf={resume} />;
-}
-
-export default Resume;
