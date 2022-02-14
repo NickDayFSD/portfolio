d0e47a7d0c8282c6bc93129a9f8e541610735efb
NickDayFSD <Nick.Day.FSD@gmail.com>
2022-02-14 13:40:46 -0800

    dep-fix: removed prop-types from unused component

--- src/components/pdf/PdfViewer.jsx
+++ src/components/pdf/PdfViewer.jsx
@@ -1,17 +1,13 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { Document, Page } from 'react-pdf';
-import PropTypes from 'prop-types';
 
 const PdfViewer = ({ pdf }) => {
   return (
-    <Document file={ pdf } options={{ workerSrc: '/pdf.worker.js ' }}>
-      <Page pageNumber={1}/>
+    <Document file={pdf} options={{ workerSrc: '/pdf.worker.js ' }}>
+      <Page pageNumber={1} />
     </Document>
   );
-};
-
-PdfViewer.propTypes = {
-  pdf: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default PdfViewer;
